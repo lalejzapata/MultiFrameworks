c8a75862c922bf93fa4fe8b35e9eb367e84ac49c
Luis Zapata <lalejzapata@falabella.cl>
2024-09-30 17:02:28 -0300

    Remove unused code

--- Package.swift
+++ Package.swift
@@ -11,9 +11,6 @@ let package = Package(
             name: "MultiFrameworksPackage",
             targets: ["MultiFrameworks", "FrameworkA", "FrameworkB"]
         )],
-//    dependencies: [
-//        .package(path: "FrameworkA/FrameworkA")
-//    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
